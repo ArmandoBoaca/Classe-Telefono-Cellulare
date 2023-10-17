70d170f79673b897dfebb10e76e1fa465babcd18
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-17 13:38:50 +0200

    feat: add attributes and constructor to Cellulare.java

--- src/Cellulare.java
+++ src/Cellulare.java
@@ -1,2 +1,12 @@
-public class Cellulare {
+public class Cellulare extends Telefono{
+    private String oS;
+
+    public Cellulare(String marca, String modello, String numero, String operatore, String os){
+        super(marca, modello, numero, operatore);
+        this.oS = os;
+    }
+
+    
+
+
 }
