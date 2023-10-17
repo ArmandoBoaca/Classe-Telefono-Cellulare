dc1eb59d4b5b139259cdfb17900d5cec15967d19
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-17 19:44:44 +0200

    test: Added test for cellulare class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        Telefono t1 = new Telefono("Apple", "iPhone 13", "+393457659809", "Vodafone");
+        Telefono t1 = new Telefono("Vodafone", "Fisso 2.0", "+0461659809", "Vodafone");
         System.out.println(t1.toString());
+
+        Cellulare c1 = new Cellulare("Apple", "iPhone 13", "+393457657236","Vodafone","iOS");
+        System.out.println(c1.toString());
+        Cellulare c2 = new Cellulare(c1);
+        c2.setOperatore("Fastweb");
+        c2.setNumero("Nessuna SIM inserita");
+        System.out.println(c2.toString());
     }
 }
